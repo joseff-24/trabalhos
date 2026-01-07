869f8275c9cc75b98b6537ec68054fedaab2d229
joseff-24 <jose1234miguelpenedogoncalves@gmail.com>
2026-01-07 23:10:51 +0000

    Add password generator script

---
+++ gerador.py
@@ -0,0 +1,9 @@
+#criador de palavras passe automatico
+
+print("Bem vindo ao criador de palavras-passe")
+print("1- Gerar nova palavra passe")
+print("2- ver as suas palavras passe")
+print("3- excluir palavras passe")
+print("4- Sair do programa")
+
+int(input("escolhe uma opcão de 0 a 4"))
