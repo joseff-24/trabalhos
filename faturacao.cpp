c9f4554d1f889a487549fec1ca80a72f5e5fb7bf
joseff-24 <jose1234miguelpenedogoncalves@gmail.com>
2026-01-23 12:12:14 +0000

    Update faturacao.cpp

--- faturacao.cpp
+++ faturacao.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
-
+// para cin e cout
 #include <vector>
 // para utilizar vectors
 #include <fstream>
 // para guardar ficheiros
 #include <string>
 // para utilizar strings
-#include <fstream>
+#include <ctime>
+// para data e hora
+
+using namespace std;
+// para evitar utilizar std::cout
+
+struct Fatura {
+    string nomeCliente
+    string nomeEmpresa
